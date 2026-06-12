eb9e5eac8f1bece5a1f5750061da7a52eaf64203
MonaTahvildari <monatahvildari@yahoo.com>
2026-06-12 19:29:25 +0200

    Fix: Update task count from 11 to 10 in stats

--- js/ui.js
+++ js/ui.js
@@ -194,7 +194,7 @@ const UI = (function() {
 
                     <div class="card-footer">
                         <span class="stat">${Object.keys(player.dailyLogs || {}).length} logged</span>
-                        <span class="stat">${Object.values(player.dailyLogs || {}).reduce((sum, log) => sum + (log.completedCount || 0), 0) || 0}/${Object.keys(player.dailyLogs || {}).length * 11 || 0} tasks</span>
+                        <span class="stat">${Object.values(player.dailyLogs || {}).reduce((sum, log) => sum + (log.completedCount || 0), 0) || 0}/${Object.keys(player.dailyLogs || {}).length * 10 || 0} tasks</span>
                     </div>
                 </div>
             `;
